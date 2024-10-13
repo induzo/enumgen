4db383652b3857292c88be37d83ab60198963a14
Vincent Serpoul <vincent@serpoul.com>
2024-10-13 16:55:25 +0800

    chore: add fullpath

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"flag"
 	"log/slog"
+	"os"
 	"strings"
 
 	"github.com/induzo/enumgen"
 )
 
 func main() {
-	genPath := flag.String("path", "./cmd/testdata", "Path to generate files")
+	genPath := flag.String("path", "./testdata", "Path to generate files")
 	packageName := flag.String("package", "book", "Package name")
 	enumTypeName := flag.String("enum", "Rating", "Enum type name")
 	enumTypeShortName := flag.String("short", "rat", "Enum type short name")
@@ -26,7 +27,24 @@ func main() {
 		EnumValues:        enumValues,
 	}
 
-	files, errG := enumgen.GenerateFiles(*genPath, data)
+	// Get the current working directory (the one `go generate` runs in).
+	cwd, errD := os.Getwd()
+	if errD != nil {
+		slog.Error("error getting current working directory", slog.Any("err", errD))
+		os.Exit(1)
+	}
+
+	path := cwd + `/` + *genPath
+
+	slog.Info("generating files",
+		slog.String("path", path),
+		slog.String("package", *packageName),
+		slog.String("enum", *enumTypeName),
+		slog.String("short", *enumTypeShortName),
+		slog.String("values", strings.Join(enumValues, ", ")),
+	)
+
+	files, errG := enumgen.GenerateFiles(path, data)
 	if errG != nil {
 		slog.Error("error generating files", slog.Any("err", errG))
 
